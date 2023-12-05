c86b4dee8053233a39221d9e6cf4e69183f45c3d
XERXES <eng.asghari75@gmail.com>
2023-12-05 16:57:30 +0330

    form added

---
+++ src/Components/InputForm.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./InputForm.module.css";
+
+export default function InputForm() {
+    return (
+        <form className={styles.form} action="#">
+            <div className={styles.form__inputsContainer}>
+                <label className={styles.form__label} htmlFor="TopText">top text</label>
+                <input 
+                    id="TopText" 
+                    type="text" 
+                    name="topText" 
+                    className={styles.form__input}
+                    placeholder="top text"
+                    />
+
+                <label 
+                    className={styles.form__label} htmlFor="bottomText">top text</label>
+                <input 
+                    id="bottomText" 
+                    type="text" 
+                    name="bottomText" 
+                    className={styles.form__input}
+                    placeholder="bottom text"
+                    />
+            </div>
+
+            <input type="submit" value="Get a new meme image" className={styles.form__submit}/>
+        </form>
+    )
+}
